2ee3ed6c19aad8c64111d9154491e52ddab06106
Michael Acquah <heallingjesus@gmail.com>
2025-08-01 20:34:43 +0000

    2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,14 @@
+const zus = (num) =>{if (num > 5){
+    console.log('No argument');
+}
+
+else if(num === 4){
+    console.log('Argument found');
+}
+
+else console.log('Arguments found');};
+
+
+console.log(zus(6));
+console.log(zus(4));
+console.log(zus(3));
